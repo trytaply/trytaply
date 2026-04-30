65e8797e0dd0d9b687bcdecd594bb6b4eb77c2a8
Josef Hamed <josefhamed123@gmail.com>
2026-04-30 22:17:18 +0200

    CORS fuer alle erlaubt und /health Test-Route

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { initialize } = require('./db/oracle');
 
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());;
 app.use(express.json());
 
 app.use('/api/auth',        require('./routes/auth'));
